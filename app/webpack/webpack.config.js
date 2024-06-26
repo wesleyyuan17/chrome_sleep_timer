32b95c957dbf38eff531377dd8580c7096edcc3f
wesleyyuan17 <wesley.yuan1746@gmail.com>
2024-06-25 22:18:21 -0400

    setup workspace, get script to work in plugin

---
+++ app/webpack/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+  mode: "production",
+  entry: {
+    background: path.resolve(__dirname, "..", "src", "background.tsx"),
+    popup: path.resolve(__dirname, "..", "src", "popup.tsx"),
+  },
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+      test: /\.tsx?$/,
+      loader: "ts-loader",
+      exclude: /node_modules/,
+      },
+      {
+        test: /\.css?$/,
+        loader: "css-loader",
+        exclude: /node_modules/,
+        },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+        patterns: [{from: ".", to: ".", context: "public"}]
+    }),
+  ],
+};
